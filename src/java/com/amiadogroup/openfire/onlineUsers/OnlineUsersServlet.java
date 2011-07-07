2aeee667d876c231a5ffda0a1f7def7a25678be3
Michael Weibel <michael@openflex.net>
2011-07-07 10:32:09 +0200

    onlineUsers plugin now supports displaying a list of online users

--- src/java/com/amiadogroup/openfire/onlineUsers/OnlineUsersServlet.java
+++ src/java/com/amiadogroup/openfire/onlineUsers/OnlineUsersServlet.java
@@ -33,9 +33,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.jivesoftware.admin.AuthCheckFilter;
 import org.jivesoftware.openfire.SessionManager;
 import org.jivesoftware.openfire.session.ClientSession;
+import org.jivesoftware.util.JiveGlobals;
 
 /**
- * Servlet for serving the online users count.
+ * Servlet for serving the online users count and the list of online users
  * 
  * @author Michael Weibel <michael.weibel@amiadogroup.com>
  */
@@ -48,26 +49,63 @@ public class OnlineUsersServlet extends HttpServlet {
 	/**
 	 * Initialize servlet & add exclude to authcheck for
 	 * http://yourserver:9090/plugins/onlineusers.
+	 * If plugin.onlineUsers.list.disableAuth is set, also add an exclude to authcheck for
+	 * http://yourserver:9090/plugins/onlineusers/list.
 	 */
 	public void init(ServletConfig servletConfig) throws ServletException {
 		super.init(servletConfig);
 
 		AuthCheckFilter.addExclude("onlineusers");
+		if (JiveGlobals.getBooleanProperty("plugin.onlineUsers.list.disableAuth", false)) {
+			AuthCheckFilter.addExclude("onlineusers/list");
+		}
 	}
 
 	/**
-	 * Get request on the online users plugin. Serve the number of plugins.
+	 * Get request on the online users plugin. Serve the number of users or the list of users.
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
 
-		Collection<ClientSession> sessions = SessionManager.getInstance()
-				.getSessions();
-		Set<String> users = new HashSet<String>(sessions.size());
-		for (ClientSession session : sessions) {
-			users.add(session.getAddress().toBareJID());
+		Set<String> users = getOnlineUsers();
+		if (request.getPathInfo().indexOf("/list") != -1) {
+			displayUserList(response, users);
+		} else {
+			displayUserCount(response, users);
 		}
+	}
 
+	/**
+	 * Displays the users lists as a JSON array
+	 * @param response
+	 * @param users
+	 * @throws IOException
+	 */
+	private void displayUserList(HttpServletResponse response, Set<String> users) throws IOException {
+		StringBuilder userList = new StringBuilder();
+		userList.append('[');
+		if (users.size() > 0) {
+			for (String user : users) {
+				userList.append("\"" + user +"\",");
+			}
+			userList.deleteCharAt(userList.length() - 1);
+		}
+		userList.append(']');
+		
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		out.println(userList);
+		out.flush();
+	}
+
+	/**
+	 * Display user count
+	 * @param response
+	 * @param users
+	 * @throws IOException
+	 */
+	private void displayUserCount(HttpServletResponse response,
+			Set<String> users) throws IOException {
 		response.setContentType("text/plain");
 		PrintWriter out = response.getWriter();
 		out.println(String.valueOf(users.size()));
@@ -75,7 +113,21 @@ public class OnlineUsersServlet extends HttpServlet {
 	}
 
 	/**
-	 * Destroy - remove the exclude filter
+	 * Get online users list
+	 * @return online users list as a set
+	 */
+	private Set<String> getOnlineUsers() {
+		Collection<ClientSession> sessions = SessionManager.getInstance()
+				.getSessions();
+		Set<String> users = new HashSet<String>(sessions.size());
+		for (ClientSession session : sessions) {
+			users.add(session.getAddress().toBareJID());
+		}
+		return users;
+	}
+
+	/**
+	 * Destroy - remove the exclude filters
 	 */
 	@Override
 	public void destroy() {
@@ -83,5 +135,6 @@ public class OnlineUsersServlet extends HttpServlet {
 
 		// Release the excluded URL
 		AuthCheckFilter.removeExclude("onlineusers");
+		AuthCheckFilter.removeExclude("onlineusers/list");
 	}
 }
